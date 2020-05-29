9d8c168296d50cb3b7c01c18e1a1de817f33c3b1
Oskar Wielanowski <oskar.wielanowski@oeconnection.com>
2020-05-29 23:28:09 +0200

    AsyncTest

---
+++ AsyncTest/AsyncTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetGWB();
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+
+        public static async Task GetGWB()
+        {
+            HttpClient hc = new HttpClient();
+            await hc.GetAsync("http://geekswithblogs.net/Default.aspx");
+            Console.WriteLine("Pobrałem zawartość");
+        }
+
+    }
+}
